344ada3e654983a8d8553c6cccbef767ecb9c411
Nardy Michelle <nardy.micheal@icloud.com>
2026-09-22 11:11:04 +0300

    Fix mobile full-width layout

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 // page / component
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex items-center justify-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex items-center justify-center flex-col overflow-hidden w-full mx-auto px-0 sm:px-10">
       <Welcome />
     </main>
   );
